e61371ad49f0271ec6085d8059502b8526117af2
AlenaKarpolenko <alenac190590@mail.ru>
2022-06-28 02:05:32 +0700

    added

---
+++ src/game.js
@@ -0,0 +1,20 @@
+/* eslint-disable max-classes-per-file */
+class Game {
+	start() {
+	  console.log('game started');
+	}
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
+
+
+import Character from './domain.js';
+export default Game;
+export {GameSavingData, readGameSaving, writeGameSaving};
